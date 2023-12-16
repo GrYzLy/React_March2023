e86cd8ee20cb46b96ad5a4da415349b1ec2ccb99
bartosz.rzemek@gmail.com <bartosz.rzemek@codecool.com>
2023-12-16 13:11:15 +0100

    CounterClass

--- src/Counter/CounterClass.js
+++ src/Counter/CounterClass.js
@@ -4,21 +4,26 @@ class CounterClass extends Component {
     constructor () {
         super()
         this.counter = 0
+
+        this.state = {
+            clickCounter : 0
+        }
     }
 
 
 incrementThisCounter()
 {
     console.log(this.counter)
-    this.counter = this.counter + 1
+    this.setState({clickCounter : this.state.clickCounter + 1})
+    this.counter = this.counter +  1
     console.log(this.counter)
 }
 
 render() {
     return (
         <div>
-            <h1>Counter:</h1>
-            <button onClick={this.incrementThisCounter()}>+1</button>
+            <h1>Counter: {this.state.clickCounter}</h1>
+            <button onClick={() => this.incrementThisCounter()}>+1</button>
         </div>
     )
 }
